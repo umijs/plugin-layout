a799567f1adaf386242c78569309eb1a6512f178
愚道 <tingzhao.ytz@antfin.com>
2020-02-21 12:13:50 +0800

    feat: support runtime patchMenus

--- example/app.ts
+++ example/app.ts
@@ -1,11 +1,20 @@
 export async function getInitialState() {
   return {
-    name: 'umijs',
+    name: "umijs"
   };
 }
 
 export const layout = {
   logout: () => {
-    alert('退出登陆成功');
+    alert("退出登陆成功");
   },
+  patchMenus: (menus: any) => {
+    return [
+      ...menus,
+      {
+        name: "自定义",
+        path: "https://bigfish.alipay.com/"
+      }
+    ];
+  }
 };
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Link, useModel } from 'umi';
-import pathToRegexp from 'path-to-regexp';
-import history from '@@/history';
-import { formatMessage } from 'umi-plugin-locale';
-import ProLayout from '@ant-design/pro-layout';
-import './style.less';
-import ErrorBoundary from '../component/ErrorBoundary';
-import useRightContent from './useRightContent';
-import { WithExceptionOpChildren } from '../component/Exception';
-import getLayoutConfigFromRoute from '../utils/getLayoutConfigFromRoute';
-import getMenuDataFromRoutes from '../utils/getMenuFromRoute';
-import logo from '../assets/logo.svg';
+import React from "react";
+import { Link, useModel } from "umi";
+import pathToRegexp from "path-to-regexp";
+import history from "@@/history";
+import { formatMessage } from "umi-plugin-locale";
+import ProLayout from "@ant-design/pro-layout";
+import "./style.less";
+import ErrorBoundary from "../component/ErrorBoundary";
+import useRightContent from "./useRightContent";
+import { WithExceptionOpChildren } from "../component/Exception";
+import getLayoutConfigFromRoute from "../utils/getLayoutConfigFromRoute";
+import getMenuDataFromRoutes from "../utils/getMenuFromRoute";
+import { MenuItem } from "../types/interface.d";
+import logo from "../assets/logo.svg";
 
 const BasicLayout = (props: any) => {
   const { children, userConfig, location } = props;
   const { initialState, loading } = (useModel &&
-    useModel('@@initialState')) || { initialState: undefined, loading: false }; // plugin-initial-state 未开启
-  const _routes = require('@@/router').routes;
+    useModel("@@initialState")) || { initialState: undefined, loading: false }; // plugin-initial-state 未开启
+  const _routes = require("@@/router").routes;
   const rightContentRender = useRightContent(userConfig, loading, initialState);
   const layoutConfig = getLayoutConfigFromRoute(_routes);
-  const menus = getMenuDataFromRoutes(_routes[0].routes);
+  const patchMenus: (ms: MenuItem[]) => MenuItem[] =
+    userConfig.patchMenus || ((ms: MenuItem[]): MenuItem[] => ms);
+  const menus = patchMenus(getMenuDataFromRoutes(_routes[0].routes));
 
   // layout 是否渲染相关
   const pathName = location.pathname;
   const layoutRender: any = {};
 
   // 动态路由匹配
-  const currentMatchPaths = Object.keys(layoutConfig).filter(item => pathToRegexp(`${item}(.*)`).test(pathName));
+  const currentMatchPaths = Object.keys(layoutConfig).filter(item =>
+    pathToRegexp(`${item}(.*)`).test(pathName)
+  );
 
-  const currentPathConfig = currentMatchPaths.length ? layoutConfig[currentMatchPaths[currentMatchPaths.length - 1]] : undefined;
+  const currentPathConfig = currentMatchPaths.length
+    ? layoutConfig[currentMatchPaths[currentMatchPaths.length - 1]]
+    : undefined;
 
   if (currentPathConfig && currentPathConfig.hideMenu) {
     layoutRender.menuRender = false;
@@ -47,19 +54,17 @@ const BasicLayout = (props: any) => {
       onMenuHeaderClick={e => {
         e.stopPropagation();
         e.preventDefault();
-        history.push('/');
+        history.push("/");
       }}
       menu={{ locale: userConfig.locale }}
       menuDataRender={() => menus}
       formatMessage={formatMessage}
-      logo={
-        initialState?.avatar || logo
-      }
+      logo={initialState?.avatar || logo}
       menuItemRender={(menuItemProps, defaultDom) => {
         if (menuItemProps.isUrl || menuItemProps.children) {
           return defaultDom;
         }
-        return <Link to={menuItemProps.path}>{defaultDom}</Link>;
+        return <Link to={menuItemProps.path || ""}>{defaultDom}</Link>;
       }}
       disableContentMargin
       rightContentRender={rightContentRender}
@@ -69,7 +74,9 @@ const BasicLayout = (props: any) => {
       {...props}
       {...layoutRender}
     >
-      <ErrorBoundary>{WithExceptionOpChildren(children, currentPathConfig)}</ErrorBoundary>
+      <ErrorBoundary>
+        {WithExceptionOpChildren(children, currentPathConfig)}
+      </ErrorBoundary>
     </ProLayout>
   );
 };
