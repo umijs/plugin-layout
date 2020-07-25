33ce58c5e31a29ef59ca4f3d0874f6034406d316
Berkay GÜRE <bgure@hotmail.com.tr>
2020-07-25 21:41:50 +0300

    fix: fixes i18n issue on Exceptions

--- src/locale/zh-CN.ts
+++ src/locale/zh-CN.ts
@@ -1,4 +1,8 @@
 export default {
   'layout.global.error.title': '出错了，可点击右下角进行反馈',
   'layout.global.error.stack': '错误堆栈',
+  'layout.global.exceptions.403.description': '抱歉，你无权访问该页面',
+  'layout.global.exceptions.403.action': '返回首页',
+  'layout.global.exceptions.500.description': '抱歉，服务器出错了',
+  'layout.global.exceptions.500.action': '返回首页',
 };
